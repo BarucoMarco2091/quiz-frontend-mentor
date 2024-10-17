ff8fb3565b3b572a204a808c0ad185fae0cc232d
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-10-17 18:52:05 -0300

    css

---
+++ script/acscript.js
@@ -0,0 +1,32 @@
+const accessibilityData = [
+    {
+        question: "What does 'WCAG' stand for?",
+        alternatives: ["Web Content Accessibility Guidelines", "Web Compliance Accessibility Guide", "Web Content Accessibility Goals", "Website Compliance and Accessibility Guidelines"],
+        correct: 0,
+    },
+    {
+        question: "Which element is used to provide alternative text for images for screen reader users?",
+        alternatives: ["<alt>", "<figcaption>", "<description>", "<img alt='description'>"],
+        correct: 3,
+    },
+    {
+        question: "What does ARIA stand for in web development?",
+        alternatives: ["Accessible Rich Internet Applications", "Advanced Responsive Internet Assistance", "Accessible Responsive Internet Applications", "Automated Responsive Internet Actions"],
+        correct: 0,
+    },
+    {
+        question: "Which of the following is not a principle of the WCAG?",
+        alternatives: ["Perceivable", "Dependable", "Operable", "Understandable"],
+        correct: 1,
+    },
+    {
+        question: "Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?",
+        alternatives: ["3:1", "4.5:1", "7:1", "2:1"],
+        correct: 1,
+    },
+    {
+        question: "Which of the following elements is inherently focusable, meaning it can receive focus without a 'tabindex' attribute?",
+        alternatives: ["<div>", "<span>", "<a href='...'>", "<p>"],
+        correct: 2,
+    },
+];
---
+++ script/jsscript.js
@@ -0,0 +1,32 @@
+const javascriptData = [
+    {
+        question: "Which syntax is correct to output 'Hello World' in an alert box?",
+        alternatives: ["alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');", "msgBox('Hello World');"],
+        correct: 2,
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        alternatives: ["call function myFunction()", "call myFunction()", "myFunction()", "execute myFunction()"],
+        correct: 2,
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        alternatives: ["if i = 5 then", "if (i == 5)", "if i == 5", "if i = 5"],
+        correct: 1,
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        alternatives: ["if (i <> 5)", "if i =! 5 then", "if (i != 5)", "if i not = 5"],
+        correct: 1,
+    },
+    {
+        question: "How does a FOR loop start?",
+        alternatives: ["for (i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)", "for (i = 0; i <= 5; i++)"],
+        alternatives: 3,
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        alternatives: ["var colors = (1:'red', 2:'green', 3:'blue')", "var colors = ['red', 'green', 'blue']", "var colors = 'red', 'green', 'blue'", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')"],
+        correct: 1,
+    },
+];
